8402ce9f803ed91a15c141b74b8919172a740bb7
Alexander <alexandrov@resprojects.ru>
2019-07-02 02:42:16 +0300

    Next home tasks is done:
    1) *transliteration

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,9 +1,23 @@
 package ru.skillbranch.devintensive.utils
 
 import ru.skillbranch.devintensive.models.User
+import java.lang.StringBuilder
 import java.util.*
 
 object Utils {
+
+    private val translitMap = mapOf(
+                            "а" to "a", "б" to "b", "в" to "v",
+                            "г" to "g", "д" to "d", "е" to "e",
+                            "ё" to "e", "ж" to "zh", "з" to "z",
+                            "и" to "i", "й" to "i", "к" to "k",
+                            "л" to "l", "м" to "m", "н" to "n",
+                            "о" to "o", "п" to "p", "р" to "r",
+                            "с" to "s", "т" to "t", "у" to "u",
+                            "ф" to "f", "х" to "h", "ц" to "c",
+                            "ч" to "ch", "ш" to "sh", "щ" to "sh'",
+                            "ъ" to "", "ы" to "i", "ь" to "",
+                            "э" to "e", "ю" to "yu", "я" to "ya")
 
     fun parseFullName(fullName: String?): Pair<String?, String?> {
         val parts : List<String>? = fullName?.split(" ")
@@ -19,6 +33,38 @@ object Utils {
             return null
         }
         return "${if (!firstName.isNullOrBlank()) firstName.toUpperCase()[0] else ""}${if (!lastName.isNullOrBlank()) lastName.toUpperCase()[0] else ""}"
+    }
+
+    fun transliteration(payload: String, divider: String = " "): String {
+        val sb = StringBuilder()
+        for (c in payload) {
+            with(sb) {
+                when(c) {
+                    ' ' -> append(divider)
+                    else -> {
+                        when(translitMap[c.toString().toLowerCase()]) {
+                            null -> append(c)
+                            else -> {
+                                if (c.isUpperCase()) {
+                                    val res = translitMap[c.toString().toLowerCase()]
+                                    if (res?.length!! > 1) {
+                                        append(res[0].toUpperCase())
+                                        for (i in 1 until res.length) {
+                                            append(res[i])
+                                        }
+                                    } else {
+                                        append(translitMap[c.toString().toLowerCase()]?.toUpperCase())
+                                    }
+                                } else {
+                                    append(translitMap[c.toString()])
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return sb.toString()
     }
 
     class Builder {
