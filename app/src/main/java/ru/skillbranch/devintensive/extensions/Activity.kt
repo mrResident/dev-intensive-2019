ad08e213cb2392f5bf3b6ea2d103391cc2cb3493
Alexander <alexandrov@resprojects.ru>
2019-07-21 17:54:39 +0300

    Tasks are completed
    1) **Activity.isKeyboardOpen Activity.isKeyboardClosed - Реализация extension

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -2,7 +2,11 @@ package ru.skillbranch.devintensive.extensions
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Rect
+import android.util.TypedValue
+import android.view.View
 import android.view.inputmethod.InputMethodManager
+import kotlin.math.roundToInt
 
 fun Activity.hideKeyboard() {
     val view = this.currentFocus
@@ -10,4 +14,28 @@ fun Activity.hideKeyboard() {
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
         imm?.hideSoftInputFromWindow(v.windowToken, 0)
     }
+}
+
+fun Activity.getRootView(): View {
+    return findViewById(android.R.id.content)
+}
+
+fun Context.convertDpToPx(dp: Float): Float {
+    return TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        dp,
+        this.resources.displayMetrics
+    )
+}
+
+fun Activity.isKeyboardOpen(): Boolean {
+    val visibleBounds = Rect()
+    this.getRootView().getWindowVisibleDisplayFrame(visibleBounds)
+    val heightDiff = getRootView().height - visibleBounds.height()
+    val marginOfError = this.convertDpToPx(50F).roundToInt()
+    return heightDiff > marginOfError
+}
+
+fun Activity.isKeyboardClosed(): Boolean {
+    return !this.isKeyboardOpen()
 }
